9f5dfb106c7b80a525c24cb023954bf8b54d25fc
sddrouet@espe.edu.ec <sddrouet@espe.edu.ec>
2024-02-04 23:05:49 -0500

    librerias y gestion de archivos

--- ProyectoU2_IngresosEgresos/js/categories.js
+++ ProyectoU2_IngresosEgresos/js/categories.js
@@ -34,6 +34,7 @@ const initDataTable = async () => {
 
 const listUsers = async () => {
     try {
+        /*
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
         const users = await response.json();
 
@@ -54,6 +55,30 @@ const listUsers = async () => {
                 </tr>`;
         });
         tableBody_categoria.innerHTML = content;
+        */
+
+        const response = await fetch("../temp/categorias.json");
+        const users = await response.json();
+
+        let content = ``;
+        users.forEach((user, index) => {
+            content += `
+                        <tr>
+                            <td>${index + 1}</td>
+                            <td>${user.nombreCategoria}</td>
+                            <td>
+                                <a href="#" class="btn btn-primary btn-circle btn-sm">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+
+                                <a href="#" class="btn btn-danger btn-circle btn-sm">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>`;
+        });
+        tableBody_categoria.innerHTML = content;
+
     } catch (ex) {
         alert(ex);
     }
